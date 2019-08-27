5e2c1b2a8cfe023d8487df98f8cb95b704d5272c
Mariana Azevedo <mariana@bsi.ufla.br>
2019-08-26 23:59:59 -0300

    Adding question on chp 6.

--- src/main/java/io/github/mariazevedo88/javaoca/chp/six/INDEX.md
+++ src/main/java/io/github/mariazevedo88/javaoca/chp/six/INDEX.md
@@ -7,6 +7,7 @@
 #### Package chp.six.caelum.methods
 
 - [Question01.java](https://github.com/mariazevedo88/java-certification-oca/blob/master/src/main/java/io/github/mariazevedo88/javaoca/chp/six/caelum/methods/Question01.java)
+- [Question02.java](https://github.com/mariazevedo88/java-certification-oca/blob/master/src/main/java/io/github/mariazevedo88/javaoca/chp/six/caelum/methods/Question02.java)
 
 
 ### Exercises from Book OCA-Oracle Certified Associate Java SE 8 Programmer I Study Guide Exam 1Z0-808
--- src/main/java/io/github/mariazevedo88/javaoca/chp/six/caelum/methods/Question02.java
+++ src/main/java/io/github/mariazevedo88/javaoca/chp/six/caelum/methods/Question02.java
@@ -2,55 +2,55 @@ package io.github.mariazevedo88.javaoca.chp.six.caelum.methods;
 
 /**
  * @author Mariana Azevedo
- * @since 21/08/2019
+ * @since 26/08/2019
  * 
- * Question 1.6.1 (page 221) - Choose the appropriate option when attempting to compile 
+ * Question 1.6.2 (page 222) - Choose the appropriate option when attempting to compile 
  * and run the following file:
  * 
  * class A {
  *	  public static void main(String[] args) {
  *		 x(args.length);
  *	  }
- *    static int x(final int l) {
- *    	 for(int i=0;i<100;i++) {
- *          switch (i) {
- *             case l:
- *                System.out.println(l);
- *                if(l==i) return;
- *    	       case 0:
- *                System.out.println(0);
- *          }
- *       }
- *       System.out.println("end");
- *       return -1;
- *    }
+ *	  static int x(final int l) {
+ *		 for(int i=0; i<100; i++) {
+ *			switch(i) {
+ *				case 1:
+ *					System.out.println(l);
+ *					if(l==i) return 3;
+ *				case 0:
+ *					System.out.println(0);
+ *			}
+ *		 }
+ *		 System.out.println("End");
+ *		 return -1;
+ *	  }
  * }
  * 
- * a) Doesn't compile. (x) 
+ * a) Doesn't compile. 
  * b) Compiles and, when run with five parameters, prints 0, 5, and end.
  * c) Compiles and, when run with five parameters, prints 0, 5, -1, and end.
  * d) Compiles and, when run with five parameters, prints 0 and 5.
- * e) Compiles and, when run with five parameters, prints 0, 5 and -1.
- * f) Compiles and, when run with five parameters, prints 0, 5, 0, and end.
+ * e) Compiles and, when run with five parameters, prints 0, 5, and -1.
+ * f) Compiles and, when run with five parameters, prints 0, 5, 0, and end. (x)
  * g) Compiles and, when run with five parameters, prints 0, 5, 0, -1, and end.
  * h) Compiles and, when run with five parameters, prints 0 and 5.
  * i) Compiles and, when run with five parameters, prints 0, 5, 0, and -1.
  */
-class Question01 {
+class Question02 {
 	public static void main(String[] args) {
 		x(args.length);
 	}
 	static int x(final int l) {
-		for(int i=0;i<100;i++) {
-			switch (i) {
-				//case l:
-					//System.out.println(l);
-					//if(l==i) return;
+		for(int i=0; i<100; i++) {
+			switch(i) {
+				case 1:
+					System.out.println(l);
+					if(l==i) return 3;
 				case 0:
 					System.out.println(0);
 			}
 		}
-		System.out.println("end");
+		System.out.println("End");
 		return -1;
 	}
 }
